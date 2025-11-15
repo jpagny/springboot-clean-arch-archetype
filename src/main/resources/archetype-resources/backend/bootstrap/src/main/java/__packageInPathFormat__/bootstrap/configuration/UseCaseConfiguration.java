c22565aad7476d3af6ed824bdab3e736105eb182
Pagny Jérôme <pagny.jerome@gmail.com>
2025-11-15 13:23:54 +0100

    fix: rename package to English name

--- src/main/resources/archetype-resources/backend/bootstrap/src/main/java/__packageInPathFormat__/bootstrap/configuration/UseCaseConfiguration.java
+++ src/main/resources/archetype-resources/backend/bootstrap/src/main/java/__packageInPathFormat__/bootstrap/configuration/UseCaseConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.bootstrap.configuration;
+package ${package}.bootstrap.configuration;
 
 import ${package}.domain.core.example.ports.input.CreateExampleUseCase;
 import ${package}.domain.core.example.usecases.CreateExampleUseCaseImpl;
